3736db2fe0af01b15115cda9b9d3bfa9099d882d
Susan <susan.adelokiki@andela.co>
2015-05-13 18:41:13 +0100

    Refactored app folder

---
+++ app/modules/homeview.js
@@ -0,0 +1,8 @@
+var app = angular.module('Andejobs', ['ngMaterial']);
+
+app.controller('AndejobsCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+}]);
