635daa4e8a842a7d0bd5d1fc99c209af68d48082
Susan <susan.adelokiki@andela.co>
2015-05-14 22:19:06 +0100

    Refactored

--- app/modules/app.js
+++ app/modules/app.js
@@ -10,25 +10,49 @@ app.controller('AndejobsCtrl', function($scope) {
     $scope.jobs = [{
       title: 'Software Developer',
       location: 'Lagos, Nigeria',
-      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+      description: "Do you have what it takes to become a world-class developer? If so, consider applying. The Andela Fellowship is a paid opportunity to work with some of the world’s leading technology companies. We are currently accepting applications at our campus in Lagos, Nigeria.",
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7))
     }, {
       title: 'Technical Recruiter',
       location: 'Lagos, Nigeria',
-      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+      description: 'The technical recruiter is responsible for sourcing, recruiting, and screening applicants for a variety of technical positions, including software engineers, system administrators, computer programmers, and database administrators.',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) 
     }, {
       title: 'Product Designer',
       location: 'Lagos, Nigeria',
-      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+      description:'Industrial designers work from design briefs to create design solutions for new products that are innovative, practical and suitable.',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) 
     }, {
       title: 'Documentarian',
       location: 'Lagos, Nigeria',
-      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+      description: 'The Documentarian is the primary organizer and overseer of visually recording (photo and video) chapter events and experiences for the purposes of documenting history and creating publicity materials',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) 
     }, {
       title: 'Operations',
       location: 'Lagos, Nigeria',
+      description: '',
       closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
     }];
-  });
+     $scope.toastPosition = {
+    bottom: false,
+    top: true,
+    left: false,
+    right: true
+  };
+  $scope.getToastPosition = function() {
+    return Object.keys($scope.toastPosition)
+      .filter(function(pos) { return $scope.toastPosition[pos]; })
+      .join(' ');
+  };
+    $scope.showCustomToast = function() {
+    $mdToast.show({
+      controller: 'AndejobsCtrl',
+      templateUrl: 'toast-template.html',
+      hideDelay: 6000,
+      position: $scope.getToastPosition()
+    });
+  };
+});
 
 app.config(['$routeProvider', function($routeProvider){
   $routeProvider
@@ -46,6 +70,10 @@ app.config(['$routeProvider', function($routeProvider){
     })
     .when('/jobs',{
       templateUrl:'views/jobs.view.html',
+      controller:'AndejobsCtrl'
+    })
+    .when('/submit',{
+      templateUrl:'views/submit.view.html',
       controller:'AndejobsCtrl'
     })
 
