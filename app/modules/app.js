e4fe338bb8e90e5c57ae597247f26b0bcabde831
Susan <susan.adelokiki@andela.co>
2015-05-14 15:34:39 +0100

    Refactored Layout

--- app/modules/app.js
+++ app/modules/app.js
@@ -4,6 +4,31 @@ var app = angular.module('Andejobs', ['ngMaterial','ngRoute']);
 app.controller('AndejobsCtrl', ['$scope', function($scope){
 
   }]);
+app.controller('AndejobsCtrl', function($scope) {
+    var imagePath = 'https://material.angularjs.org/img/list/60.jpeg';
+    var date = new Date();
+    $scope.jobs = [{
+      title: 'Software Developer',
+      location: 'Lagos, Nigeria',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+    }, {
+      title: 'Technical Recruiter',
+      location: 'Lagos, Nigeria',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+    }, {
+      title: 'Product Designer',
+      location: 'Lagos, Nigeria',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+    }, {
+      title: 'Documentarian',
+      location: 'Lagos, Nigeria',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+    }, {
+      title: 'Operations',
+      location: 'Lagos, Nigeria',
+      closing_date: "Application deadline: " + date.toString(date.setDate(date.getDate() + 7)) ,
+    }];
+  });
 
 app.config(['$routeProvider', function($routeProvider){
   $routeProvider
@@ -18,6 +43,10 @@ app.config(['$routeProvider', function($routeProvider){
     .when('/signup',{
     templateUrl:'views/signup.view.html',
     controller:'AndejobsCtrl'
+    })
+    .when('/jobs',{
+      templateUrl:'views/jobs.view.html',
+      controller:'AndejobsCtrl'
     })
 
 }])
--- app/modules/homeview.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('Andejobs', ['ngMaterial']);
-
-app.controller('AndejobsCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
- 
-}]);
