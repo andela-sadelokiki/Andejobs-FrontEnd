a2b9c84dd4efa4011a4dc4c476f119793b31b9fc
Susan <susan.adelokiki@andela.co>
2015-05-20 12:33:37 +0100

    Refactor

--- app/controllers/application.js
+++
@@ -1,7 +0,0 @@
-app.controller('ApplicationsCtrl', function($scope, ApplicationService){
-  $scope.submitApplication = function(){
-    ApplicationService.submit($scope.application);
-  };
-
-  $scope.allApplications = []
-})
